d497aee355510b9c8e62421379ac04bc9f2ec20e
Renée Cruz <reneebasscruz@gmail.com>
2020-04-10 23:20:39 -0400

    load user's score on end screen and high score in local storage

--- end.js
+++ end.js
@@ -1,5 +1,8 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
     // console.log(username.value);
